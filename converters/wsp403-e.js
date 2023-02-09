1a5ccf93b582e851c2d3f7a5f2c8315cb3735727
czuio <czuio@users.noreply.github.com>
2023-02-09 13:42:10 +1100

    Create wsp403-e.js

---
+++ converters/wsp403-e.js
@@ -0,0 +1,24 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const definition = {
+        zigbeeModel: ['WSP403-E'],
+        model: 'WSP403-E',
+        vendor: 'OWON',
+        description: 'Smart Plug',
+        fromZigbee: [fz.on_off],
+        toZigbee: [tz.on_off],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(endpoint);
+        },
+        exposes: [e.switch()],
+};
+
+module.exports = definition;
